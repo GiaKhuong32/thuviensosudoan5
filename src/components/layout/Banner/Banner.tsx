232a1a31c29c413bafed09ed69b6f642b4970b33
helloworld2346 <baolam.dev2346@gmail.com>
2026-08-09 09:27:56 +0700

    feat: create public layout

---
+++ src/components/layout/Banner/Banner.tsx
@@ -0,0 +1,15 @@
+function Banner() {
+  return (
+    <section className="bg-primary text-white">
+      <div className="mx-auto max-w-7xl px-4 py-10">
+        <h2 className="text-2xl font-bold">Thư Viện Số Sư Đoàn 5</h2>
+
+        <p className="mt-2 text-sm">
+          Kho tài liệu và sách điện tử của Sư Đoàn 5.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
