009ec1ead8a636206806fd0fe71fb568808a8ae1
helloworld2346 <baolam.dev2346@gmail.com>
2026-08-09 08:58:56 +0700

    chore: configure project foundation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       colors: {
         primary: {
-          DEFAULT: "#D32029",
-          hover: "#B71C1C",
+          DEFAULT: "#007A3F",
+          hover: "#006633",
         },
       },
+
       fontSize: {
-        base: ["14px", "1.5"],
+        xs: ["14px", "1.5"],
         sm: ["14px", "1.5"],
+        base: ["14px", "1.5"],
       },
     },
   },
+
   plugins: [],
-}
+};
